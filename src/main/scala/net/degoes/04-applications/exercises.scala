160b94a3efd6a98638215c3c7888f86d570923d0
Nikita Gridinskiy <nikita.gridinskiy@gmail.com>
2018-11-21 17:48:38 -0800

    Wooh done with day 4

--- src/main/scala/net/degoes/04-applications/exercises.scala
+++ src/main/scala/net/degoes/04-applications/exercises.scala
@@ -25,8 +25,8 @@ object exercises extends App {
     processor : (URL, String) => IO[E, A]): IO[Nothing, Crawl[E, A]] = {
 
     def loop(seeds: Set[URL], ref: Ref[(Crawl[E, A], Set[URL])]): IO[Nothing, Unit] =
-      IO.traverse(seeds.flatMap(router)) { url =>
-        getURL(url).redeem(
+      seeds.flatMap(router).foldLeft(IO.unit) { (acc, url) =>
+        acc *> getURL(url).redeem(
           _ => IO.unit,
           html =>
             /// For comprehensions cannot be tail recursive because they have the final map!!!
@@ -42,13 +42,13 @@ object exercises extends App {
 
             ref.modify {
               case (crawl0, visited) =>
-                visited -> (crawl0 |+| crawl1, visited ++ urls)
+                (visited, (crawl0 |+| crawl1, visited ++ urls))
             }.flatMap { old =>
               loop(urls.toSet -- old, ref)
             }
           }
         )
-      }.void : IO[Nothing, Unit]
+      } : IO[Nothing, Unit]
 
     for {
       ref <- Ref(mzero[Crawl[E, A]] -> seeds)
@@ -81,8 +81,42 @@ object exercises extends App {
     seeds     : Set[URL],
     router    : URL => Set[URL],
     processor : (URL, String) => IO[E, A],
-    getURL    : URL => IO[Exception, String] = getURL(_)): IO[Nothing, Crawl[E, A]] =
-      ???
+    getURL    : URL => IO[Exception, String] = getURL): IO[Nothing, Crawl[E, A]] = {
+
+
+    def loop(seeds: Set[URL], ref: Ref[(Crawl[E, A], Set[URL])]): IO[Nothing, Unit] =
+      seeds.flatMap(router).foldLeft(IO.unit) { (acc, url) =>
+        acc *> getURL(url).redeem(
+          _ => IO.unit,
+          html =>
+            /// For comprehensions cannot be tail recursive because they have the final map!!!
+
+            //            for {
+            //              crawl1 <- processor(url, html).redeemPure(Crawl(_, mzero[A]), Crawl(mzero[E], _))
+            //              urls <- IO.now(extractURLs(url, html))
+            //              crawl2 <- loop(urls.toSet -- visited, crawl0 |+| crawl1, visited ++ urls)
+            //            } yield crawl2
+
+            processor(url, html).redeemPure(Crawl(_, mzero[A]), Crawl(mzero[E], _)).flatMap { crawl1 =>
+              val urls = extractURLs(url, html)
+
+              ref.modify {
+                case (crawl0, visited) =>
+                  (visited, (crawl0 |+| crawl1, visited ++ urls))
+              }.flatMap { old =>
+                loop(urls.toSet -- old, ref)
+              }
+            }
+        )
+      } : IO[Nothing, Unit]
+
+    for {
+      ref <- Ref(mzero[Crawl[E, A]] -> seeds)
+      _ <- loop(seeds, ref)
+      tuple <- ref.get
+    } yield tuple._1
+
+  }
 
   //
   // EXERCISE 4
@@ -90,7 +124,8 @@ object exercises extends App {
   // Create a type class to describe `printLine` and `readLine`.
   //
   trait Console[F[_]] {
-    ???
+    def printLine(line: String): F[Unit]
+    def readLine(): F[String]
   }
   object Console {
     def apply[F[_]](implicit F: Console[F]): Console[F] = F
@@ -102,8 +137,8 @@ object exercises extends App {
   // Implement helper methods called `printLine` and `readLine` that work with
   // any `F[_]` that supports the `Console` effect.
   //
-  def printLine[F[_]: Console](line: String): F[Unit] = ???
-  def readLine[F[_]: Console]: F[String] = ???
+  def printLine[F[_]: Console](line: String): F[Unit] = Console[F].printLine(line)
+  def readLine[F[_]: Console]: F[String] = Console[F].readLine()
 
   //
   // EXERCISE 6
@@ -114,8 +149,8 @@ object exercises extends App {
   //
   implicit def ConsoleIO[E]: Console[IO[E, ?]] =
     new Console[IO[E, ?]] {
-      def printLine(line: String): IO[E, Unit] = ???
-      def readLine: IO[E, String] = ???
+      def printLine(line: String): IO[E, Unit] = IO.sync(println(line))
+      def readLine: IO[E, String] = IO.sync(scala.io.StdIn.readLine())
     }
 
   //
@@ -133,7 +168,7 @@ object exercises extends App {
   def nextInt[F[_]: Random](max: Int): F[Int] = Random[F].nextInt(max)
   implicit def RandomIO[E]: Random[IO[E, ?]] =
     new Random[IO[E, ?]] {
-      def nextInt(max: Int): IO[E, Int] = ???
+      def nextInt(max: Int): IO[E, Int] = IO.sync(scala.util.Random.nextInt(max))
     }
 
   //
@@ -143,7 +178,13 @@ object exercises extends App {
   // requiring only the capability to perform `Console` and `Random` effects.
   //
   def myGame[F[_]: Console: Random: Monad]: F[Unit] =
-    ???
+    for {
+      name  <- getName[F]
+      word  <- chooseWord[F]
+      state <- State(name, Set.empty, word).point[F]
+      _     <- renderState[F](state)
+      _     <- gameLoop[F](state)
+    } yield ()
 
   case class State(name: String, guesses: Set[Char], word: String) {
     def failures: Int = (guesses -- word.toSet).size
@@ -154,7 +195,19 @@ object exercises extends App {
   }
 
   def gameLoop[F[_]: Console: Monad](state: State): F[State] =
-    ???
+  for {
+    guess <- getChoice[F]
+    state <- state.copy(guesses = state.guesses + guess).point[F]
+    _     <- renderState[F](state)
+    state <- if (state.playerLost) {
+      printLine[F](s"Sorry, ${state.name}, you lost!") *> state.point[F]
+    } else if (state.playerWon) {
+      printLine[F]("Congrats you have won! now buy yourself a cookie and pat your cat on the head, cause he is smarter than you.") *> state.point[F]
+    } else {
+      val guessedRight = state.word.contains(guess)
+      printLine[F](if (guessedRight) s"Good job, ${state.name}, keep going" else "You are fucking wrong, try again loser.") *> gameLoop[F](state)
+    }
+  } yield state
 
   def renderState[F[_]: Console](state: State): F[Unit] = {
     //
@@ -177,13 +230,22 @@ object exercises extends App {
   }
 
   def getChoice[F[_]: Console: Monad]: F[Char] =
-    ???
+    for {
+      _ <- printLine[F]("Please guess a letter:")
+      line <- readLine[F]
+      letter <- line.trim.toLowerCase.headOption match {
+        case Some(letter) =>
+          letter.point[F]
+        case None =>
+          printLine[F]("You did not enter a letter.") *> getChoice[F]
+      }
+    } yield letter
 
   def getName[F[_]: Console: Apply]: F[String] =
-    ???
+    printLine[F]("Please enter your name:") *> readLine[F]
 
   def chooseWord[F[_]: Random: Functor]: F[String] =
-    ???
+    nextInt[F](Dictionary.length).map(Dictionary.lift(_).getOrElse("buggy"))
 
   val Dictionary = List("aaron", "abelian", "ability", "about", "abstract", "abstract", "abstraction", "accurately", "adamek", "add", "adjacent", "adjoint", "adjunction", "adjunctions", "after", "after", "again", "ahrens", "albeit", "algebra", "algebra", "algebraic", "all", "all", "allegories", "almost", "already", "also", "american", "among", "amount", "ams", "an", "an", "analysis", "analytic", "and", "and", "andre", "any", "anyone", "apart", "apologetic", "appears", "applicability", "applications", "applications", "applied", "apply", "applying", "applying", "approach", "archetypical", "archetypical", "are", "areas", "argument", "arising", "aristotle", "arrowsmorphism", "article", "arxiv13026946", "arxiv13030584", "as", "as", "aspect", "assumed", "at", "attempts", "audience", "august", "awodey", "axiom", "axiomatic", "axiomatized", "axioms", "back", "barr", "barry", "basic", "basic", "be", "beginners", "beginning", "behind", "being", "benedikt", "benjamin", "best", "better", "between", "bicategories", "binary", "bodo", "book", "borceux", "both", "both", "bourbaki", "bowdoin", "brash", "brendan", "build", "built", "but", "but", "by", "called", "cambridge", "can", "cardinal", "carlos", "carnap", "case", "cases", "categorial", "categorical", "categorical", "categories", "categories", "categorification", "categorize", "category", "category", "cats", "catsters", "central", "certain", "changes", "charles", "cheng", "chicago", "chiefly", "chopin", "chris", "cite", "clash", "classes", "classical", "closed", "coend", "coin", "colimit", "colin", "collection", "collections", "comparing", "completion", "composed", "composition", "computational", "computer", "computing", "concept", "concepts", "concepts", "conceptual", "concrete", "confronted", "consideration", "considers", "consistently", "construction", "constructions", "content", "contents", "context", "context", "contexts", "continues", "continuous", "contrast", "contributed", "contributions", "cooper", "correctness", "costas", "count", "course", "cover", "covering", "current", "currently", "david", "decategorification", "deducing", "define", "defined", "defining", "definition", "definitions", "der", "derives", "described", "describing", "description", "descriptions", "detailed", "development", "dictum", "did", "different", "dimensions", "directed", "discovered", "discovery", "discuss", "discussed", "discussion", "discussion", "disparage", "disservice", "do", "does", "driving", "drossos", "duality", "dvi", "each", "easy", "ed", "edges", "edit", "edition", "eilenberg", "eilenbergmaclane", "elementary", "elementary", "elements", "elementwise", "elephant", "ellis", "else", "embedding", "embodiment", "embryonic", "emily", "end", "enthusiastic", "equations", "equivalence", "equivalences", "equivalences", "etc", "etcs", "eugenia", "even", "eventually", "everything", "evident", "example", "examples", "examples", "except", "excused", "exist", "exists", "exposure", "expressed", "expressiveness", "extension", "extra", "f", "fact", "fair", "families", "far", "feeds", "feeling", "finds", "finite", "first", "flourished", "focuses", "folklore", "follows", "fong", "for", "for", "force", "forced", "foremost", "form", "formalizes", "formulated", "forthcoming", "found", "foundation", "foundations", "foundations", "francis", "free", "freyd", "freydmitchell", "from", "functions", "functor", "functor", "functors", "fundamental", "further", "gabrielulmer", "general", "general", "generalized", "generalizes", "geometry", "geometry", "george", "geroch", "get", "gift", "give", "given", "going", "goldblatt", "grandis", "graph", "gray", "grothendieck", "ground", "group", "groupoid", "grp", "guide", "göttingen", "had", "handbook", "handful", "handle", "harper", "has", "have", "he", "here", "here", "herrlich", "higher", "higher", "higherdimensional", "highlevel", "hilberts", "his", "historical", "historically", "history", "history", "holistic", "holland", "home", "homomorphisms", "homotopy", "homotopy", "horizontal", "horst", "however", "i", "idea", "ideas", "ieke", "if", "if", "illustrated", "important", "in", "in", "inaccessible", "inadmissible", "include", "includes", "including", "indeed", "indexes", "infinite", "informal", "initial", "innocent", "instance", "instead", "instiki", "interacting", "internal", "intersection", "into", "introduce", "introduced", "introduces", "introducing", "introduction", "introduction", "introductory", "intuitions", "invitation", "is", "isbell", "isbn", "isomorphisms", "it", "it", "its", "itself", "ive", "j", "jaap", "jacob", "jiri", "johnstone", "joy", "jstor", "just", "kan", "kant", "kapulkin", "kashiwara", "kind", "kinds", "kleins", "kmorphisms", "ktransfors", "kℕ", "la", "lagatta", "lane", "language", "large", "last", "later", "later", "latest", "lauda", "lawvere", "lawveres", "lead", "leads", "least", "lectures", "led", "leinster", "lemma", "lemmas", "level", "library", "lifting", "likewise", "limit", "limits", "link", "linked", "links", "list", "literally", "logic", "logic", "logically", "logische", "long", "lurie", "mac", "maclane", "made", "major", "make", "manifest", "many", "many", "mappings", "maps", "marco", "masaki", "material", "mathct0305049", "mathematical", "mathematical", "mathematician", "mathematician", "mathematics", "mathematics", "mathematicsbrit", "may", "mclarty", "mclartythe", "means", "meet", "membership", "methods", "michael", "misleading", "mitchell", "models", "models", "moerdijk", "monad", "monadicity", "monographs", "monoid", "more", "morphisms", "most", "mostly", "motivation", "motivations", "much", "much", "music", "must", "myriads", "named", "natural", "natural", "naturally", "navigation", "ncategory", "necessary", "need", "never", "new", "nlab", "no", "no", "nocturnes", "nonconcrete", "nonsense", "nontechnical", "norman", "north", "northholland", "not", "notes", "notes", "nothing", "notion", "now", "npov", "number", "object", "objects", "obliged", "observation", "observing", "of", "on", "one", "online", "oosten", "operads", "opposed", "or", "order", "originally", "other", "other", "others", "out", "outside", "outside", "over", "packing", "page", "page", "pages", "paper", "paradigm", "pareigis", "parlance", "part", "particularly", "pdf", "pedagogical", "people", "perfect", "perhaps", "perpetrated", "perspective", "peter", "phenomenon", "phil", "philosopher", "philosophers", "philosophical", "philosophy", "physics", "physics", "pierce", "pierre", "played", "pleasure", "pointed", "poset", "possession", "power", "powered", "powerful", "pp", "preface", "prerequisite", "present", "preserving", "presheaf", "presheaves", "press", "prevail", "print", "probability", "problem", "proceedings", "process", "progression", "project", "proof", "property", "provide", "provides", "ps", "publicly", "published", "pure", "purloining", "purpose", "quite", "quiver", "rails", "rather", "reader", "realizations", "reason", "recalled", "record", "references", "reflect", "reflects", "rejected", "related", "related", "relation", "relation", "relations", "representable", "reprints", "reproduce", "resistance", "rests", "results", "reveals", "reverse", "revised", "revisions", "revisions", "rezk", "riehl", "robert", "role", "row", "ruby", "running", "same", "samuel", "saunders", "say", "scedrov", "schanuel", "schapira", "school", "sci", "science", "scientists", "search", "see", "see", "sense", "sep", "sequence", "serious", "set", "set", "sets", "sets", "sheaf", "sheaves", "shortly", "show", "shulman", "similar", "simon", "simple", "simplified", "simply", "simpson", "since", "single", "site", "situations", "sketches", "skip", "small", "so", "society", "some", "some", "sometimes", "sophisticated", "sophistication", "source", "space", "speak", "special", "specific", "specifically", "speculative", "spivak", "sprache", "stage", "standard", "statements", "steenrod", "stephen", "steps", "steve", "still", "stop", "strecker", "structural", "structuralism", "structure", "structures", "students", "study", "studying", "subjects", "such", "suggest", "summer", "supported", "supports", "symposium", "syntax", "tac", "taken", "talk", "tannaka", "tautological", "technique", "tend", "tends", "term", "terminology", "ternary", "tex", "textbook", "textbooks", "texts", "than", "that", "the", "the", "their", "their", "them", "themselves", "then", "theorem", "theorems", "theorems", "theoretic", "theoretical", "theories", "theorist", "theory", "theory", "there", "there", "these", "these", "they", "thinking", "this", "this", "thought", "through", "throughout", "thus", "time", "to", "tom", "tone", "too", "toolset", "top", "topics", "topoi", "topological", "topology", "topologyhomotopy", "topos", "topos", "toposes", "toposes", "transactions", "transformation", "transformations", "trinitarianism", "trinity", "triple", "triples", "trivial", "trivially", "true", "turns", "two", "two", "type", "typically", "uncountable", "under", "under", "understood", "unification", "unify", "unions", "univalent", "universal", "universal", "universes", "university", "use", "used", "useful", "using", "usual", "van", "variants", "various", "vast", "vect", "versatile", "video", "videos", "viewpoint", "views", "vol", "vol", "vs", "was", "way", "we", "wealth", "web", "wells", "were", "what", "when", "when", "where", "which", "while", "whole", "whose", "will", "willerton", "william", "willingness", "with", "witticism", "words", "working", "working", "would", "writes", "xfy", "xfygzxgfz", "xy", "yoneda", "york1964", "youtube")
 
@@ -192,7 +254,7 @@ object exercises extends App {
   //
   // Instantiate the polymorphic game to the `IO[Nothing, ?]` type.
   //
-  val myGameIO: IO[Nothing, Unit] = myGame ?
+  lazy val myGameIO: IO[Nothing, Unit] = myGame[IO[Nothing, ?]]
 
   //
   // EXERCISE 10
@@ -201,8 +263,13 @@ object exercises extends App {
   // read from the console), a log of output (that has been written to the
   // console), and a list of "random" numbers.
   //
-  case class TestData(/* ??? */ ) {
-    def renderOutput: String = ???
+  case class TestData(
+                     input: List[String],
+                     output: List[String],
+                     random: List[Int]
+                     ) {
+    def renderOutput: String =
+      output.reverse.mkString("\n")
   }
 
   //
@@ -220,9 +287,12 @@ object exercises extends App {
       def bind[A, B](fa: TestIO[E,A])(f: A => TestIO[E,B]): TestIO[E,B] =
         TestIO(fa.run.flatMap(f.andThen(_.run)))
 
-      def printLine(line: String): TestIO[E,Unit] = ???
-      def readLine: TestIO[E, String] = ???
-      def nextInt(max: Int): TestIO[E,Int] = ???
+      def printLine(line: String): TestIO[E,Unit] =
+        TestIO(ref.update(d => d.copy(output = line :: d.output)).void)
+      def readLine: TestIO[E, String] =
+        TestIO(ref.modify(d => d.input.head -> d.copy(input = d.input.drop(1))))
+      def nextInt(max: Int): TestIO[E,Int] =
+        TestIO(ref.modify(d => (d.random.head, d.copy(random = d.random.drop(1)))))
     }
 
   //
@@ -233,11 +303,12 @@ object exercises extends App {
   //
   def testGame(data: TestData): IO[Nothing, TestData] =
     for {
-      ref   <- Ref(data)
-      _     <- ({
-                 ???
-               }: TestIO[Nothing, Unit]).run
-      data  <- ref.get
+      ref <- Ref(data)
+      _ <- ({
+        implicit val F = createTestInstance(ref)
+        myGame[TestIO[Nothing, ?]]
+      }: TestIO[Nothing, Unit]).run
+      data <- ref.get
     } yield data
 
   //
@@ -246,7 +317,9 @@ object exercises extends App {
   // Create some test data for a trial run of the game.
   //
   val GameTest1 = testGame(TestData(
-    /* ??? */
+    input = List("John", "a", "r", "o", "n"),
+    output = Nil,
+    random = List(0)
   )).map(_.renderOutput)
 
   final case class Crawl[E, A](error: E, value: A) {
--- src/main/scala/net/degoes/FunctionalScala.scala
+++ src/main/scala/net/degoes/FunctionalScala.scala
@@ -4,6 +4,65 @@ import scalaz.zio._
 import scalaz.zio.console._
 
 object FunctionalScala extends App {
+  /*
+  Tick tack toe, start with the data model.
+   */
+
+  case class Position private(x: Int, y: Int)
+  object Position {
+    def apply(x: Int, y: Int): Option[Position] = {
+      if((0 to 3).contains(x) && (0 to 3).contains(y))
+        Some(new Position(x, y))
+      else
+        None
+    }
+  }
+
+  trait Piece {}
+
+  final object Piece {
+    final case object X extends Piece
+    final case object O extends Piece
+  }
+
+  case class Action(position: Position, piece: Piece)
+
+  trait Query[+A] { self =>
+    type S
+
+    def zero: A
+
+    def step(s: S, action: Action): Either[S, A]
+
+    final def zip[B] (that: Query[B]): Query[(A, B)] = {
+      ???
+    }
+  }
+
+  object Query {
+  }
+
+
+  case class Board(actions: List[Action]) {
+    def isValid(a: Action): Boolean = {
+      !actions.exists(_.position == a.position)
+    }
+
+    def applyMove(a: Action): Option[Board] = {
+      if(isValid(a))
+        Some(copy(actions = a :: actions))
+      else
+        None
+    }
+
+    def hasWon(p: Piece): Boolean = {
+      ???
+    }
+
+  }
+
+
+
   def run(args: List[String]): IO[Nothing, ExitStatus] =
     (for {
       _ <- putStrLn("Hello World!")
